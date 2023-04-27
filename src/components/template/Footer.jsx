ed2257631f87967adce090dc7dffc6c11fea867d
dev-edsonlopes <dev_edson@outlook.com>
2023-04-27 11:38:06 -0300

    projeto concluído COMMIT

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className='footer'>
+      <span>
+         desenvolvido com <i className='text-danger'>react</i>
+      </span>
+    </footer>
